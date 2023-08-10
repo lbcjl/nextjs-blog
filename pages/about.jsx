87e6e30ce32488a277e344b69783df939afd651e
Edwards <1285704653@qq.com>
2023-08-10 15:31:51 +0800

    third commit

---
+++ pages/about.jsx
@@ -0,0 +1,21 @@
+import Layout from '../components/layout'
+
+export default function About() {
+	return (
+		<Layout>
+			<div>About</div>
+		</Layout>
+	)
+}
+
+export async function getStaticProps(context) {
+	return {
+		redirect: {
+			destination: '/',
+			permanent: true, // triggers 308
+		},
+	}
+	// return {
+	// 	notFound: true, // triggers 404
+	// }
+}
