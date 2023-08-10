993517d3f432522d3db17bc5ff6b9980be33b1a4
Edwards <1285704653@qq.com>
2023-08-10 13:30:51 +0800

    change name

--- components/layout.jsx
+++ components/layout.jsx
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Edward Liu'
+const name = 'Edward Code'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
