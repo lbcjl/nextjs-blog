0ffdfc77e33ea2f445d600d888d5fce76e64cbc2
Edwards <1285704653@qq.com>
2023-08-10 13:28:32 +0800

    change name

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
 				<title>{siteTitle}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
-				<p>Hello, i'm Edward</p>
+				<p>Hello, i'm Edward Code</p>
 				<p>
 					(This is a sample website - you’ll be building a site like this on{' '}
 					<a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
